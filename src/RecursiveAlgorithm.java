ddb67a6ec4b684dc7efd6d692fe4040009f21741
jun16017108 <jun.16017108@gmail.com>
2023-11-24 10:25:08 +0900

    Initial commit

---
+++ src/RecursiveAlgorithm.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class RecursiveAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(factorial(4));
+
+
+    }
+
+    public static int factorial(int n) {
+        int sum = 0;
+        if (n==1) return 1;
+        else {
+            for(int i=1; i<=n; i++) {
+                sum += i;
+
+            }
+            return factorial(n-1) + sum;
+        }
+
+    }
+}
